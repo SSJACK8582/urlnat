2ecb917df3e6565fbe03fb0868f410eebea04e6f
SSJACK8582 <1805330812@qq.com>
2023-12-22 21:00:25 +0800

    Update router.go

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"urlnat/controller"
 )
 
@@ -11,5 +12,8 @@ func Router() *gin.Engine {
 	router.POST("/delete", controller.DeleteUrlNat)
 	router.POST("/update", controller.UpdateUrlNat)
 	router.GET("/code/*path", controller.GetUrlNat)
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"code": 200, "msg": "start success"})
+	})
 	return router
 }
